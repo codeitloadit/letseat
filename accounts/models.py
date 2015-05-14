9687858cabbe8d13fa6d30cb5b4c05ad64d21fdc
codeitloadit <brian@codeitload.it>
2015-05-14 05:28:51 +0000

    Remove accounts app, update bottom nav

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User)
-#     department = models.CharField(max_length=100)
